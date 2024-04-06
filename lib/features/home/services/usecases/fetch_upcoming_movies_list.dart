5da7132f1931bb81d1dc532cf231dfb30aa05a94
Kashif Umar <kashifumar349@gmail.com>
2024-04-07 01:17:14 +0500

    modifications saving

--- lib/features/home/services/usecases/fetch_upcoming_movies_list.dart
+++ lib/features/home/services/usecases/fetch_upcoming_movies_list.dart
@@ -3,7 +3,7 @@ import 'package:movieapp/features/home/services/data/upcoming_movies_params.dart
 import 'package:movieapp/features/home/services/repositroy/home_repository.dart';
 import 'package:movieapp/models/upcoming_movies_model.dart';
 import 'package:movieapp/network/exceptions/netwrok_exceptions.dart';
-import 'package:movieapp/utils/generic_use_case.dart';
+import 'package:movieapp/utils/usecase.dart';
 
 class FetchUpcomingMoviesUsecase
     extends UseCase<UpcomingMovies, UpcomingMoviesParams> {
